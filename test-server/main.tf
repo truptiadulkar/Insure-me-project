29587411e639cee37247109dd3fc53ddd702e12f
root <root@ip-172-31-44-4.ap-south-1.compute.internal>
2023-06-08 22:29:22 +0000

    changes done

--- test-server/main.tf
+++ test-server/main.tf
@@ -1,24 +1,30 @@
 resource "aws_instance" "test-server" {
-  ami           = "ami-049a62eb90480f276" 
-  instance_type = "t2.micro" 
-  key_name = "keypairdoc"
-  vpc_security_group_ids= ["sg-0ddc4a835e0d5e85c"]
-  connection {
-    type     = "ssh"
-    user     = "ubuntu"
-    private_key = file("./keypairdoc.pem")
-    host     = self.public_ip
-  }
-  provisioner "remote-exec" {
-    inline = [ "sleep 60 && echo 'Instance ready'"]
-  }
+  ami                    = "ami-049a62eb90480f276"
+  instance_type          = "t2.micro"
+  key_name               = "keypairdoc"
+  vpc_security_group_ids = ["sg-0ddc4a835e0d5e85c"]
+
   tags = {
     Name = "test-server"
   }
-  provisioner "local-exec" {
-        command = " echo ${aws_instance.test-server.public_ip} > inventory "
+ 
+  provisioner "remote-exec" {
+    inline = [ "sleep 60", "echo 'Instance ready'"]
   }
-   provisioner "local-exec" {
-  command = "ansible-playbook /var/lib/jenkins/workspace/Insure-me-project/Deploy.yml "
-  } 
+
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"
+    private_key = file("./keypairdoc.pem")
+    host        = self.public_ip 
+  }
+   
+  provisioner "local-exec" {
+   command = " echo ${aws_instance.test-server.public_ip} > inventory "
+  }
+
+  provisioner "local-exec" {
+    command = "ansible-playbook /var/lib/jenkins/workspace/Insure-me-project/Deploy.yml "
+  }
 }
+
