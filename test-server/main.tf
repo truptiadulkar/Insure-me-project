9b2e5616ebf4d703c8e53c8833d187d3b45f304e
root <root@ip-172-31-44-4.ap-south-1.compute.internal>
2023-06-09 08:44:48 +0000

    changes done

--- test-server/main.tf
+++ test-server/main.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "test-server" {
+ "aws_instance" "test-server" {
   ami                    = "ami-049a62eb90480f276"
   instance_type          = "t2.micro"
   key_name               = "keypairdoc"
@@ -7,11 +7,14 @@ resource "aws_instance" "test-server" {
   tags = {
     Name = "test-server"
   }
- 
-  provisioner "local-exec" {
-   command = [ "sleep 60", "echo 'Instance ready'"]
+  
+   provisioner "remote-exec" {
+    inline = [
+      "sleep 60",
+      "echo 'Instance ready'"
+    ]
   }
-
+  
   connection {
     type        = "ssh"
     user        = "ubuntu"
@@ -20,11 +23,11 @@ resource "aws_instance" "test-server" {
   }
    
   provisioner "local-exec" {
-   command = " echo ${aws_instance.test-server.public_ip} > inventory "
+    command = "echo ${aws_instance.test-server.public_ip} > inventory"
   }
 
   provisioner "local-exec" {
-    command = "ansible-playbook /var/lib/jenkins/workspace/Insure-me-project/Deploy.yml "
+    command = "ansible-playbook /var/lib/jenkins/workspace/Insure-me-project/test-server/Deploy.yml"
   }
 }
 
--- test-server/provider.tf
+++ test-server/provider.tf
@@ -1,5 +1,1 @@
-provider "aws" {
-region     = "ap-south-1" 
-access_key= "AKIA2L4DQOEDDPNYKJEP"
-secret_key= "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"
-}
+provider
